ed2fe0d77ff715e1b80c14f0a7133d0916953d8d
Admir <admir.ferhatovic@gmail.com>
2018-03-11 15:54:26 +0100

    initial commit

---
+++ src/main/java/com/ius/foodyapi/FoodyApiApplication.java
@@ -0,0 +1,12 @@
+package com.ius.foodyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodyApiApplication.class, args);
+	}
+}
