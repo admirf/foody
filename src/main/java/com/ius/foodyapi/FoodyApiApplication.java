16c11aefe518134274a35a1dca640326de6209f3
Admir <admir.ferhatovic@gmail.com>
2018-03-15 21:09:01 +0100

    user repo

--- src/main/java/com/ius/foodyapi/FoodyApiApplication.java
+++ src/main/java/com/ius/foodyapi/FoodyApiApplication.java
@@ -2,10 +2,11 @@ package com.ius.foodyapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class FoodyApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FoodyApiApplication.class, args);
 	}
---
+++ src/main/java/com/ius/foodyapi/UserRepository.java
@@ -0,0 +1,8 @@
+package com.ius.foodyapi;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+}
