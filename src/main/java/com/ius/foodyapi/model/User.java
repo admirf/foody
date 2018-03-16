397d6ea28730dd40fcdde361b732e193824495b6
Benjamin Fajić <fajicb@gmail.com>
2018-03-16 12:33:26 +0100

    lab commit, setting DB

--- src/main/java/com/ius/foodyapi/model/User.java
+++ src/main/java/com/ius/foodyapi/model/User.java
@@ -1,11 +1,9 @@
-package com.ius.foodyapi;
+package com.ius.foodyapi.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -27,5 +25,21 @@ public class User {
         return String.format(
                 "User[id=%d, firstName='%s', lastName='%s']",
                 id, firstName, lastName);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
--- src/main/java/com/ius/foodyapi/repository/UsersRepository.java
+++ src/main/java/com/ius/foodyapi/repository/UsersRepository.java
@@ -1,8 +1,8 @@
-package com.ius.foodyapi;
+package com.ius.foodyapi.repository;
 
+import com.ius.foodyapi.model.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
-public interface UserRepository extends PagingAndSortingRepository<User, Long> {
-}
+public interface UsersRepository extends PagingAndSortingRepository<User, Long> { }
